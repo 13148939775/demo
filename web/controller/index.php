12a83274d35b5c438018e3bb05bfff5535e629d7
13148939775 <33994786+13148939775@users.noreply.github.com>
2017-11-28 10:59:24 +0800

    提交控制器

---
+++ web/controller/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: w1077
+ * Date: 2017/11/28
+ * Time: 10:58
+ */
+class controller{
+    public $a = 0;
+    function a(){
+        echo "这是控制器";
+    }
+}
